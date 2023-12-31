189b2d90eb5b7bdb1b6718774c5bf4cc643cd6b8
Deniz Traka <deniz.traka@deptagency.com>
2023-12-31 17:09:13 +0100

    Update Attacking.ts

--- src/schema/components/Attacking.ts
+++ src/schema/components/Attacking.ts
@@ -8,12 +8,12 @@ export class Attacking extends Schema {
     @type("number") cooldownTimer: number;
     @type("boolean") isAttacking: boolean;
 
-    constructor(power = 1, swingSpeed = 0.5, range = 1) {
+    constructor(power = 10, swingSpeed = 0.5, range = 1) {
         super();
         this.power = power;
         this.swingSpeed = swingSpeed;
         this.range = range;
-        this.cooldown = 1000 / swingSpeed;
+        this.cooldown = 1000 * swingSpeed - 100;
         this.cooldownTimer = 0;
     }
 }
